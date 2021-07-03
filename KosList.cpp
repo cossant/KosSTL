1a878df45451bb1c1bc9efea6273c00aeb33c30a
cossant <maksmaksim.2003@mail.ru>
2021-07-04 01:25:10 +0400

    Implemented delete logic for List elements. Still have no "safity rails" (no input/logical checking).

--- KosList.cpp
+++ KosList.cpp
@@ -7,26 +7,33 @@ public:
 	KosList();
 	void addline(contentType);
 	contentType line(int);
+	void deletenode(int);
 private:
 	class node
 	{
 	public:
-		node(contentType,int);
+		node(contentType);
 		node* nextNode;
 		contentType line(int);
-
+		node* linePtr(int lineIndex)
+		{
+			if (lineIndex == 0)	return this;
+			else nextNode->linePtr(lineIndex - 1);
+		}
 	private:
 		contentType data;
-		int index;
 	};
-	int nodesAmought;
 	node* startNode;
 	node* lastNode;
+	node* linePtr(int index)
+	{
+		return startNode->linePtr(index);
+	}
 };
 
 
 template<typename contentType>
-KosList<contentType>::KosList() : nodesAmought(0)
+KosList<contentType>::KosList()
 {
 	startNode = nullptr;
 	lastNode = startNode;
@@ -38,12 +45,12 @@ void KosList<contentType>::addline(contentType inpdata)
 	int* a = nullptr;
 	if (startNode != nullptr)
 	{
-		lastNode->nextNode = new node(inpdata, nodesAmought++);
+		lastNode->nextNode = new node(inpdata);
 		lastNode = lastNode->nextNode;
 	}
 	else
 	{
-		startNode = new node(inpdata, nodesAmought++);
+		startNode = new node(inpdata);
 		lastNode = startNode;
 	}
 	
@@ -52,21 +59,37 @@ void KosList<contentType>::addline(contentType inpdata)
 template <typename contentType>
 contentType KosList<contentType>::line(int nodeIndex)
 {
-	return startNode->line(nodeIndex);
+	return startNode->line(--nodeIndex);
 }
 
 template <typename contentType>
 contentType KosList<contentType>::node::line(int nodeIndex)
 {
-	if (nodeIndex == index) return data;
-	else nextNode->line(nodeIndex);
+	if (nodeIndex == -1) return data;
+	else nextNode->line(--nodeIndex);
 }
 
+
 template<typename contentType>
-KosList<contentType>::node::node(contentType initContent, int nodeIndex)
+KosList<contentType>::node::node(contentType initContent)
 {
-	index = nodeIndex;
 	nextNode = nullptr;
 	data = initContent;
 }
+
+
+
+template <typename contentType>
+void KosList<contentType>::deletenode(int index)
+{
+	node* nodeA = (index > 0) ? linePtr(index - 1) : startNode;
+	node * nodeToDelete = linePtr(index);
+	node * nodeB = (lastNode == linePtr(index)) ? nullptr : linePtr(index + 1);	
+	if (index > 0)
+		nodeA->nextNode = nodeB;
+	else
+		startNode = nodeB;
+	delete nodeToDelete;
+}
+
 
--- main.cpp
+++ main.cpp
@@ -6,9 +6,17 @@ using namespace std;
 int main()
 {
 	KosList<int> list;
+	// Adding integral value.
 	list.addline(1);
 	list.addline(4);
 	list.addline(5);
 	list.addline(2);
-	cout << list.line(2) << list.line(1)<< list.line(1) << list.line(0) << list.line(3);
+	// Displaying values by index.
+	cout << list.line(0) << list.line(1)<< list.line(2) << list.line(3) << '\n';
+	// Deleting single node from it's index.
+	list.deletenode(2);
+	// Displaying values by index.
+	cout << list.line(0);
+	cout << list.line(1);
+	cout << list.line(2);
 }
